98b151ae74948a763ceee8adcebfd6aa6cc12b62
Kevin Jordan <kevincentdeco@gmail.com>
2017-11-03 21:13:25 +0700

    3/11/17 - tpolygon is finished

--- PolygonFill/Main.Designer.vb
+++ PolygonFill/Main.Designer.vb
@@ -22,17 +22,31 @@ Partial Class Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBox
+        '
+        Me.PictureBox.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PictureBox.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(796, 540)
+        Me.PictureBox.TabIndex = 0
+        Me.PictureBox.TabStop = False
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(992, 654)
+        Me.ClientSize = New System.Drawing.Size(1126, 614)
+        Me.Controls.Add(Me.PictureBox)
         Me.Name = "Main"
         Me.Text = "Polygon Fill"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents PictureBox As PictureBox
 End Class
